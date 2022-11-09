cde55f84d310c1531ba79d6267fe01ef0a7fa452
Manh Nguyen <manhnb1911@gmail.com>
2022-11-09 08:12:13 +0700

    rename upload image->file

--- api/v1/middleware/upload.py
+++ api/v1/middleware/upload.py
@@ -3,7 +3,7 @@ from fastapi import UploadFile
 from ..exception.http import BadRequestException
 
 
-def validate_image(image: UploadFile) -> UploadFile:
-    if not image.content_type.startswith("image/"):
+def validate_image(file: UploadFile) -> UploadFile:
+    if not file.content_type.startswith("image/"):
         raise BadRequestException("File is not image")
-    return image
+    return file
